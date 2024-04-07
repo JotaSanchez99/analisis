fceb18048ef8f37bfe7a7aec8ce2c91a3bca6d4b
MrKranidos <mjk90000@gmail.com>
2024-04-07 17:24:34 -0400

    codigo tratando de hacer funcar firebase

--- app-pc/V1- Login/login.py
+++ app-pc/V1- Login/login.py
@@ -2,23 +2,46 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivymd.app import MDApp
 from kivymd.uix.button import MDRectangleFlatButton
+from google.cloud import firestore
+import firebase_admin
+from firebase_admin import credentials
+#from firebase_admin import firestore
 
 
 
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+collection = db.collection('usuarios')  # create collection
+
+#Creacion de la APP
 class MainApp(MDApp):
+
+    #Base de la app
     def build(self):
+        #Configuracion visual inicial
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "BlueGray"
+        
+        #Creacion de la pantalla con el archivo .kv externo
         return Builder.load_file('login.kv')
 
+    #Funcion para iniciar sesión
     def logger(self):
+        
+        #Datos de Login
         usuario = self.root.ids.user.text
         contraseña = self.root.ids.password.text
+        
+        #Codigo de legado - testear
         if contraseña == "12345":
             self.root.ids.welcome_label.text = f'Bienvenido {usuario}!'
         else:
             self.root.ids.welcome_label.text = "Error de contraseña"
+            
 
+    #Funcion para recuperar cuenta
     def recuperar(self):
         self.root.ids.welcome_label.text = "Bienvenido"
         self.root.ids.user.text = ""
