b03531417d4e8871f301cff9dbcd218dac5f87a7
MrKranidos <mjk90000@gmail.com>
2024-04-07 17:29:58 -0400

    Update login.py

--- app-pc/V1- Login/login.py
+++ app-pc/V1- Login/login.py
@@ -10,7 +10,7 @@ from firebase_admin import credentials
 
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
+cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 collection = db.collection('usuarios')  # create collection
